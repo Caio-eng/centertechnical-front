7c16227c4a5505fd422f3d517ad4eaccf5add80c
Caio-eng <caiopereira1998@hotmail.com>
2023-07-10 13:50:54 -0300

    Ajustes finais

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
     private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/read/1'])
+    this.router.navigate(['home'])
   }
 
   logout() {
